b6fb75c955c353a9c39af142a390cda5190acba6
jimmyliucy <526320379@qq.com>
2021-03-11 10:39:34 +0800

    160.77 reverse commit last page and next page;

--- src/main/webapp/js/operator/surgeryBefore.js
+++ src/main/webapp/js/operator/surgeryBefore.js
@@ -73,3 +73,43 @@ $(function() {
             }
         });
     }
+
+    /****************************************************
+     * last Keyword in function signature is mean
+     * 表示上一页；
+     * next Keyword in function signature is mean
+     * 表示下一页；
+     * 显示隐藏DIV域的方法：
+     *   1、$("#demo").attr("style","display:none;");//隐藏div
+     *      $("#demo").attr("style","display:block;");//显示div
+     *   2、$("#demo").css("display","none");//隐藏div
+     *      $("#demo").css("display","block");//显示div
+     *   3、$("#demo").hide();//隐藏div
+     *      $("#demo").show();//显示div
+     *   4、$("#demo").toggle(//动态显示和隐藏
+     *          function () {
+     *              $(this).attr("style","display:none;");//隐藏div
+     *          },
+     *          function () {
+     *              $(this).attr("style","display:block;");//显示div
+     *          });
+     * **************************************************/
+    function patientNextPage() {
+        $("#addPatient").css("display","none");//隐藏div
+        $("#setMMSE").css("display","block");//显示div
+    }
+
+    function mmseLastPage() {
+        $("#setMMSE").css("display","none");//
+        $("#addPatient").css("display","block");//
+    }
+
+    function mmseNextPage() {
+        $("#setMMSE").css("display","none");//
+        $("#addCam").css("display","block");//
+    }
+
+    function camLastPage() {
+        $("#addCam").css("display","none");//
+        $("#setMMSE").css("display","block");//
+    }
